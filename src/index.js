cb8562bdaea8cc24e1f385f3317e6367f026cb51
better_320 <70196050+naeun-shin@users.noreply.github.com>
2024-03-25 03:41:52 +0900

    Revert "Feature/header"

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Modal from 'react-modal';
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-Modal.setAppElement('#root');
-const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <QueryClientProvider client={queryClient}>
+  <React.StrictMode>
     <App />
-  </QueryClientProvider>
+  </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
